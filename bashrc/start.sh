9dc9037dca4a10655185b1366f593a9c8ebebe21
richelle <sutherland.richelle3@gmail.com>
2025-05-10 17:27:52 +0300

    p1 Sat May 10 17:27:52 MSK 2025

---
+++ bashrc/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# This script is used to start the bashrc configuration
+
+. ./creator_bashrc.sh
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# This script sets up the environment for the project by installing necessary dependencies and setting up the Python environment.
+
+. ./bashrc/start.sh
+. ./nvim/start.sh
