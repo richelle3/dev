e5718dde7294e14b71f865b86e35c85f6918ae7d
richelle <sutherland.richelle3@gmail.com>
2025-05-10 12:48:39 +0300

    save Sat May 10 12:48:39 MSK 2025

---
+++ nvim/src/plugins/cmp.lua
@@ -0,0 +1,61 @@
+-- Cmp configuration for nvim-cmp
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+  },
+  config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+    local ok_cmp, cmp = pcall(require, "cmp")
+    if not ok_cmp then return end
+
+    local ok_luasnip, luasnip = pcall(require, "luasnip")
+    if not ok_luasnip then return end
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+      },
+    })
+
+    -- Setup for `/` (search)
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" }
+      }
+    })
+
+    -- Setup for `:` (command line)
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" }
+      }, {
+        { name = "cmdline" }
+      })
+    })
+  end,
+}
